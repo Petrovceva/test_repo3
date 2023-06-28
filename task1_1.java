c1db56cb19a50a9c15b1b5a1f91a8d76259ee1f5
Petrovceva <123365984+Petrovceva@users.noreply.github.com>
2023-06-28 21:37:42 +0300

    Create task1_1.java

---
+++ task1_1.java
@@ -0,0 +1,12 @@
+public class task1_1 {
+    static int calculateFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result*i;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(calculateFactorial(5));    
+    }
+}
